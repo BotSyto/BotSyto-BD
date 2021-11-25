e0fd08d1ffd907d4c08106abb835d9445f6d5b56
CorfGod <omar2pocket@gmail.com>
2021-11-25 10:50:41 -0500

    25-11:1050

--- api/Tutorial.js
+++ api/Tutorial.js
@@ -2,6 +2,29 @@ const Tutorial = require("./../models/Tutorial");
 const express = require("express");
 const router = express.Router();
 
+router.post('/subirTuto',(req,res) =>{
+    let{name,description}=req.body
+    name=name.trim();
+    description=description.trim();
+    if(name==""||description==""){
+        res.json({
+            status: "FAILED",
+            message: "Hay campos vacios",
+        });
+    }else{
+        Tutorial.find({name}).then( (result) =>{
+            if(result.length){
+                res.json({
+                    status: "FAILED",
+                    message: "Ya hay un tutorial registrado con ese nombre",
+                });
+            }
+        })
+    }
+
+});
+
+
 router.get('/tutorial', (req, res) => {
     let { idTutorial } = req.body
     Tutorial.find({ idTutorial }).then((result) => {
--- models/Tutorial.js
+++ models/Tutorial.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const TutorialSchema = new Schema({
     name: String,
     description: String,
-    kind: Boolean,
 });
 
 const Tutorial = mongoose.model('Tutorial', TutorialSchema);
